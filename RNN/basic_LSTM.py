d54a39d490343cd424f77e744c8960e989e654b1
tinyfrog <tinyfrog12@hanmail.net>
2020-01-16 23:51:07 +0900

    Implement basic LSTM

--- RNN/basic_LSTM.py
+++ RNN/basic_LSTM.py
@@ -46,3 +46,76 @@ def random_training_set():
     inp = char_tensor(chunk[:-1])
     target = char_tensor(chunk[1:])
     return inp, target
+
+class RNN(nn.Module):
+    def __init__(self, input_size, embedding_size, hidden_size, output_size, num_layers=1):
+        super(RNN, self).__init__()
+        self.input_size = input_size
+        self.hidden_size = hidden_size
+        self.output_size = output_size
+        self.num_layers = num_layers
+        self.embedding_size = embedding_size
+
+        self.encoder = nn.Embedding(input_size, embedding_size)
+        self.rnn = nn.RNN(embedding_size, hidden_size, num_layers)
+        self.decoder = nn.Linear(hidden_size, output_size)
+
+    def forward(self, input, hidden):
+        out = self.encoder(input.view(1, -1))
+        out, hidden = self.rnn(out, hidden)
+        out = self.decoder(out.view(batch_size, -1))
+        return out, hidden
+
+    def init_hidden(self):
+        hidden = torch.zeros(self.num_layers, batch_size, hidden_size)
+        return hidden
+
+model = RNN(input_size=n_characters,
+            embedding_size=embedding_size,
+            hidden_size=hidden_size,
+            output_size=n_characters,
+            num_layers=2)
+
+inp = char_tensor("A")
+hidden,cell = model.init_hidden()
+out,hidden,cell = model(inp,hidden,cell)
+
+# Optimizer
+optimizer = torch.optim.Adam(model.parameters(), lr=lr)
+loss_func = nn.CrossEntropyLoss()
+
+for i in range(num_epochs):
+    total = char_tensor(randum_chunk())
+    inp = total[:-1]
+    label = total[1:]
+    hidden = model.init_hidden()
+
+    loss = torch.tensor([0]).type(torch.FloatTensor)
+    optimizer.zero_grad()
+    for j in range(chunk_len-1):
+        x = inp[j]
+        y_ = label[j].unsqueeze(0).type(torch.LongTensor)
+        y, hidden = model(x, hidden)
+        loss += loss_func(y,y_)
+
+    loss.backward()
+    optimizer.step()
+
+# Test Function
+def test():
+    start_str = "b"
+    inp = char_tensor(start_str)
+    hidden,cell = model.init_hidden()
+    x = inp
+
+    print(start_str,end="")
+    for i in range(200):
+        output,hidden,cell = model(x,hidden,cell)
+
+        output_dist = output.data.view(-1).div(0.8).exp()
+        top_i = torch.multinomial(output_dist, 1)[0]
+        predicted_char = all_characters[top_i]
+
+        print(predicted_char,end="")
+
+        x = char_tensor(predicted_char)
